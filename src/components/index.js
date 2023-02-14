86ab7c9d4da38295bc890a5e6638348b34470fbc
hailatGH <hailat.alx@gmail.com>
2023-02-14 16:29:40 +0300

    completed

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Download from "./Download";
+import Button from "./Button";
+import Features from "./Features";
+import SectionWrapper from "./SectionWrapper";
+
+export { Download, Button, Features, SectionWrapper };
